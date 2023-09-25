44970c4fb47d075a7224556a90f2b4ed318f4c44
Jaya Gupta <jgupta@GSHYD-FW1W470TXL.local>
2023-09-25 16:20:52 +0530

    Modified checkout feature

---
+++ src/main/java/com/jaya/Casestudy/Entity/Order.java
@@ -0,0 +1,84 @@
+package com.jaya.Casestudy.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "allOrders")
+public class Order {
+    @Id
+    @Column(name = "mobile")
+    private String mobile;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "total_price")
+    private double totalPrice;
+
+    public Order()  {}
+
+    public Order(String mobile, String firstName, String lastName, String email, String address, double totalPrice) {
+        this.mobile = mobile;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
