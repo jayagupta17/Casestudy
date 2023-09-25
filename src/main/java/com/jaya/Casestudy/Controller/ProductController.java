6a8ddc684f43fc4ff12f2f6fb69065385b28975a
Jaya Gupta <jgupta@GSHYD-FW1W470TXL.local>
2023-09-25 19:50:07 +0530

    Search feature

--- src/main/java/com/jaya/Casestudy/Controller/ProductController.java
+++ src/main/java/com/jaya/Casestudy/Controller/ProductController.java
@@ -5,8 +5,7 @@ import com.jaya.Casestudy.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,12 @@ public class ProductController {
     public String getAllProducts(Model model){
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
+        return "displayProducts";
+    }
+
+    @PostMapping(value = "/search")
+    public String searchProducts(@RequestParam("str") String str, Model model) {
+        model.addAttribute("products", productService.searchProducts(str));
         return "displayProducts";
     }
 }
--- src/main/java/com/jaya/Casestudy/DAO/ProductDAO.java
+++ src/main/java/com/jaya/Casestudy/DAO/ProductDAO.java
@@ -2,6 +2,13 @@ package com.jaya.Casestudy.DAO;
 
 import com.jaya.Casestudy.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductDAO extends JpaRepository<Product, Integer> {
+    @Query("select p from Product p where productName like concat('%', :str, '%')")
+    public List<Product> searchProducts(@Param("str") String str);
 }
